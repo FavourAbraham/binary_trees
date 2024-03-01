e6271cd1b1b4778c8d49f0686ad878be716db9ea
FavourAbraham <fa4abraham18@gmail.com>
2024-02-29 17:46:54 -0800

    Binary Trees

--- binary_trees.h
+++ binary_trees.h
@@ -65,7 +65,6 @@ heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-i
 void binary_tree_print(const binary_tree_t *);
 
 #endif /* BINARY_TREES_H */
